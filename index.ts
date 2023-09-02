093e232455049d3748327f515cb3966f137bcac4
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-09-02 15:00:54 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,19 +1,24 @@
 import { interval } from 'rxjs';
 import { windowCount, mergeAll, tap } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/transformation/windowcount
+// Example 1: Start new window every x items emitted
+
 //emit every 1s
-const source = interval(1000);
-const example = source.pipe(
+const srcInterval$ = interval(1000);
+const example = srcInterval$.pipe(
   //start new window every 4 emitted values
   windowCount(4),
-  tap(_ => console.log('NEW WINDOW!'))
+  tap((_) => console.log('NEW WINDOW!'))
 );
 
 const subscribeTwo = example
   .pipe(
     //window emits nested observable
     mergeAll()
-    /*
+  )
+  .subscribe((val) => console.log(val));
+/*
             output:
             "NEW WINDOW!"
             0
@@ -25,6 +30,4 @@ const subscribeTwo = example
             5
             6
             7
-          */
-  )
-  .subscribe(val => console.log(val));
+*/
